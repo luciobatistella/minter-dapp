8bd52ed4a9d1c71e32830e4f983a1eec06be68ef
Lucio Batistella <lucio.batistella@gmail.com>
2022-05-02 14:42:48 -0300

    js

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,7 +58,7 @@ const CHAIN = 'mainnet'; // only rinkeby or polygon
 const CONTRACT_NAME = 'Lettuce Hands';
 const CONTRACT_SYMBOL = 'LH';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = '0x0CC805107c573c73B003fDB054abee7Ef842BC52';
+const OWNER_ADDRESS = '0x6388cb805b4E1a1c5bf9Cd868c0926f6147C7A3b';
 const TREASURY_ADDRESS = '0xd3B87dC18f9eD56a41703CB38d633b5D3b9cff00';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.08; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
@@ -73,7 +73,7 @@ const ROYALTY_SHARE = 10000 ; // Percentage of the token price that goes to the 
 const ROYALTY_ADDRESS = "0xd3B87dC18f9eD56a41703CB38d633b5D3b9cff00"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xd3B87dC18f9eD56a41703CB38d633b5D3b9cff00"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
