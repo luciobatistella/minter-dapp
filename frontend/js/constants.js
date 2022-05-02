1fa5cd82c7d834e74d675a5ee27aaec3c8a21cb7
Lucio Batistella <lucio.batistella@gmail.com>
2022-05-02 12:07:55 -0300

    update colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x3789B51fcC4DF14c3A71e5e7a79cFA9b0cEcA547"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_h1 = "Seja bem vindos alfaceiros!";
+const welcome_h2 = "Vem, conecta sua metamask... vou roubar toda sua grana.";
+const welcome_p = 'Esse mint vai ser um estouro! Queremos seu dinheiro alfaceiro!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
