12ebba6f724be3c8e5db8f61196e8d43f67ca268
Lucio Batistella <lucio.batistella@gmail.com>
2022-05-02 14:43:51 -0300

    js

--- backend/src/config.js
+++ backend/src/config.js
@@ -68,7 +68,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-05-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-01T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = ""; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 10000 ; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xd3B87dC18f9eD56a41703CB38d633b5D3b9cff00"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
